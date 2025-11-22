d75a978a73ee29156f45b6557f1a0a143f340077
FloortjeTjeertes <floortje@frillybits.dev>
2025-11-22 11:46:56 +0100

    💬 update hero text

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -7,13 +7,12 @@
         <h1
           class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
         >
-          Payments tool for software companies
+          Games from outer space /*place whatever catch frase needs to be here*/
         </h1>
         <p
           class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
         >
-          From checkout to global sales tax compliance, companies around the world use Flowbite to
-          simplify their payment stack.
+          making games that inspire and make you give a feeling that makes you shout:'weeeeee' /*change to actual text in the future*/
         </p>
         <a
           href="#"
